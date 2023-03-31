8a22f51b90eb802561e8191ecbf9e3e22a14495b
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-31 01:41:39 -0700

    add vite-plugin-radar with ga4 ID

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteRadar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-Q1YPY6Y7FM',
+      },
+    })
+  ],
   build: {
     outDir: 'build'
   }
